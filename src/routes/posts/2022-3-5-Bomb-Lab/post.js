0febc0611d54505b614650dda55b4efed80bf03b
jacobleonard545 <jacobleonard545@gmail.com>
2022-08-07 11:01:55 -0400

    bomb lab update

---
+++ src/routes/posts/2022-3-5-Bomb-Lab/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Binary Bomb Lab`,
+  tags: ['cybersecurity', 'reverse-engineering'],
+  spoiler: "Bomb defused. Using GDB and NSA's Ghidra.",
+  getContent: () => import('./document.mdx'),
+}
