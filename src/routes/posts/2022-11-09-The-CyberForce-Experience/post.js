fccb17ae65d8a134b20e0073bf3f39239625faa3
jacobleonard545 <jacobleonard545@gmail.com>
2022-11-09 20:00:10 -0500

    update

---
+++ src/routes/posts/2022-11-09-The-CyberForce-Experience/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `The CyberForce Experience`,
+  tags: ['cybersecurity'],
+  spoiler: "An overview of the DOE's national competition and my experience over the years.",
+  getContent: () => import('./document.mdx'),
+}
